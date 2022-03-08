a34ffeff582dcb076174de918227de9b865b3510
Larissa Araujo <larissa.jvsa@gmail.com>
2022-03-08 20:13:47 -0300

    Teste Profile: Feito

--- src/tests/Profile.test.js
+++ src/tests/Profile.test.js
@@ -5,62 +5,96 @@ import renderWithRouter from './renderWithRouter';
 // import Profile from '../pages/Profile';
 import App from '../App';
 
-const MOCK_EMAIL = 'teste@teste.com';
+const RADOM_EMAIL = 'RAMDOM@EMAIL.com';
 const MOCK_PASSWORD = '1234567';
 const BTN_LENGTH = 3;
 
 describe('Testar a página Profile', () => {
-  it.only('Teste se a página contém o email do localStorage', () => {
-    const { history, debug } = renderWithRouter(<App />);
-    // history.push('/');
-    
+  it('Teste se a página contém o email do localStorage', () => {
+    const { history } = renderWithRouter(<App />);
+
     const inputEmail = screen.getByLabelText('Email:');
     const inputPassword = screen.getByLabelText('Senha:');
     const btnLogin = screen.getByRole('button', { name: 'Entrar' });
-    // const emailText = screen.getByText(MOCK_EMAIL);
 
-    userEvent.type(inputEmail, MOCK_EMAIL);
+    userEvent.type(inputEmail, RADOM_EMAIL);
     userEvent.type(inputPassword, MOCK_PASSWORD);
-    
     userEvent.click(btnLogin);
-    debug();
+
     history.push('/profile');
-    console.log(localStorage.getItem('user'));
-    // expect(screen.getByText(MOCK_EMAIL)).toBeInTheDocument();
-    // expect(screen.getByText(MOCK_EMAIL)).toBeInTheDocument();
+
+    expect(screen.getByText(RADOM_EMAIL)).toBeInTheDocument();
   });
 
   it('Teste se a página contém três botões', () => {
     const { history } = renderWithRouter(<App />);
+
+    const inputEmail = screen.getByLabelText('Email:');
+    const inputPassword = screen.getByLabelText('Senha:');
+    const btnLogin = screen.getByRole('button', { name: 'Entrar' });
+
+    userEvent.type(inputEmail, RADOM_EMAIL);
+    userEvent.type(inputPassword, MOCK_PASSWORD);
+    userEvent.click(btnLogin);
+
     history.push('/profile');
-    expect(screen.getByRole('button')).toHaveLength(BTN_LENGTH);
+    expect(screen.getAllByRole('button')).toHaveLength(BTN_LENGTH);
   });
 
   it('Teste se o botão Done Recipes vai para a rota /done-recipes', () => {
     const { history } = renderWithRouter(<App />);
+
+    const inputEmail = screen.getByLabelText('Email:');
+    const inputPassword = screen.getByLabelText('Senha:');
+    const btnLogin = screen.getByRole('button', { name: 'Entrar' });
+
+    userEvent.type(inputEmail, RADOM_EMAIL);
+    userEvent.type(inputPassword, MOCK_PASSWORD);
+    userEvent.click(btnLogin);
+
     history.push('/profile');
-    const btnDone = screen.getByRole('button', { name: /done recipes/i });
+
+    const btnDone = screen.getByTestId('profile-done-btn');
+
     userEvent.click(btnDone);
     history.push('/done-recipes');
-    expect(screen.getByText(/rota done recipes/i)).toBeInTheDocument();
+    expect(screen.getByText('Rota Done Recipes')).toBeInTheDocument();
   });
 
   it('Teste se o botão Favorite Recipes vai para a rota /favorites-recipes', () => {
     const { history } = renderWithRouter(<App />);
+
+    const inputEmail = screen.getByLabelText('Email:');
+    const inputPassword = screen.getByLabelText('Senha:');
+    const btnLogin = screen.getByRole('button', { name: 'Entrar' });
+
+    userEvent.type(inputEmail, RADOM_EMAIL);
+    userEvent.type(inputPassword, MOCK_PASSWORD);
+    userEvent.click(btnLogin);
+
     history.push('/profile');
-    const btnFavorite = screen.getByRole('button', { name: /favorite recipes/i });
+
+    const btnFavorite = screen.getByTestId('profile-favorite-btn');
     userEvent.click(btnFavorite);
     history.push('/favorite-recipes');
-    expect(screen.getByText(/rota favorites recipes/i)).toBeInTheDocument();
+    expect(screen.getByText('Rota Favorites Recipes')).toBeInTheDocument();
   });
 
   it('Teste se o botão Logout vai para a rota /', () => {
     const { history } = renderWithRouter(<App />);
+
+    const inputEmail = screen.getByLabelText('Email:');
+    const inputPassword = screen.getByLabelText('Senha:');
+    const btnLogin = screen.getByRole('button', { name: 'Entrar' });
+
+    userEvent.type(inputEmail, RADOM_EMAIL);
+    userEvent.type(inputPassword, MOCK_PASSWORD);
+    userEvent.click(btnLogin);
+
     history.push('/profile');
 
-    const btnLogout = screen.getByRole('button', { name: /logout/i });
-
-    userEvent.click(btnLogout);
+    const btnFavorite = screen.getByTestId('profile-logout-btn');
+    userEvent.click(btnFavorite);
     history.push('/');
   });
 });
