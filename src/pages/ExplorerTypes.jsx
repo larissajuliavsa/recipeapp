5c4c5e686e3acf910be43b0634560ade313bd541
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-03-09 18:04:16 -0300

    Criacao da logica do botao "Surprise me" e da pagina service

--- src/pages/ExplorerTypes.jsx
+++ src/pages/ExplorerTypes.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Footer from '../components/Footer';
+import {
+  getAletorieRecipeDrink,
+  getAletorieRecipeFood,
+} from '../service/AletoriesRecipes';
 
-function ExplorerTypes({ history }) {
-  const btnExplore = (route) => {
-    history.push(`/explore/${route}`);
+function ExplorerTypes(props) {
+  const { history, match } = props;
+  const { params } = match;
+  const { foodsAndDrinks } = params;
+
+  const btnRedirectRoute = (route) => {
+    history.push(`/explore/${foodsAndDrinks}/${route}`);
+  };
+
+  const redirectRoute = (route) => {
+    history.push(`/${foodsAndDrinks}/${route}`);
+  };
+
+  const btnSurprise = async () => {
+    if (foodsAndDrinks === 'foods') {
+      const recipeMeal = await getAletorieRecipeFood();
+      const { meals } = recipeMeal;
+      const { idMeal } = meals[0];
+      redirectRoute(idMeal);
+    }
+    if (foodsAndDrinks === 'drinks') {
+      const recipeDrink = await getAletorieRecipeDrink();
+      const { drinks } = recipeDrink;
+      const { idDrink } = drinks[0];
+      redirectRoute(idDrink);
+    }
   };
 
   return (
@@ -13,21 +40,24 @@ function ExplorerTypes({ history }) {
       <button
         type="button"
         data-testid="explore-by-ingredient"
-        onClick={ () => btnExplore('foods') }
+        onClick={ () => btnRedirectRoute('ingredients') }
       >
         By Ingredient
       </button>
-      <button
-        type="button"
-        data-testid="explore-by-nationality"
-        onClick={ () => btnExplore('drinks') }
-      >
-        By Nationality
-      </button>
+      {
+        foodsAndDrinks === 'foods' && (
+          <button
+            type="button"
+            data-testid="explore-by-nationality"
+            onClick={ () => btnRedirectRoute('nationalities') }
+          >
+            By Nationality
+          </button>)
+      }
       <button
         type="button"
         data-testid="explore-surprise"
-        onClick={ () => btnExplore('drinks') }
+        onClick={ () => btnSurprise() }
       >
         Surprise me!
       </button>
@@ -39,7 +69,8 @@ function ExplorerTypes({ history }) {
 ExplorerTypes.propTypes = {
   history: PropTypes.shape({
     push: PropTypes.func,
-  }).isRequired,
-};
+  }),
+  params: PropTypes.shape(),
+}.isRequired;
 
 export default ExplorerTypes;
