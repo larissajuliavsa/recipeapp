8a7f8480044238f0a50830df53ec8588c6ddf9c0
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-03-09 16:06:31 -0300

    Criacao da Tela Explorer

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Home from './pages/Home';
 import Profile from './pages/Profile';
 import Done from './pages/Done';
 import Favorites from './pages/Favorites';
+import Explorer from './pages/Explorer';
 
 function App() {
   return (
@@ -25,10 +26,11 @@ function App() {
         <span>Hello Word!</span>
         <Switch>
           <Route exact path="/" render={ (props) => <Login { ...props } /> } />
-          <Route path="/foods" component={ Home } />
+          <Route exact path="/foods" component={ Home } />
           <Route exact path="/profile" component={ Profile } />
           <Route exact path="/done-recipes" component={ Done } />
           <Route exact path="/favorite-recipes" component={ Favorites } />
+          <Route exact path="/explore" render={ (props) => <Explorer { ...props } /> } />
         </Switch>
       </RecipeProvider>
     </div>
