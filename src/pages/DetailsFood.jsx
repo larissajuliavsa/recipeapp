fc19a29ddd035bca6e925ede07dcd087a2cf340f
Larissa Araujo <larissa.jvsa@gmail.com>
2022-03-12 17:52:55 -0300

    feat: Tela Details css responsivo

--- src/pages/DetailsFood.jsx
+++ src/pages/DetailsFood.jsx
@@ -5,10 +5,11 @@ import { useParams } from 'react-router-dom';
 import YouTube from 'react-youtube';
 
 import RecipeContext from '../context/RecipeContext';
+import Header from '../components/Header';
 import getDetailsFoodAPI from '../services/detailsFoodAPI';
 import getRecommendationFoodAPI from '../services/recommendationFoodAPI';
 
-import '../assets/DetailsFood.css';
+import '../assets/css/DetailsFood.css';
 import shareIcon from '../images/shareIcon.svg';
 import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 // import blackHeartIcon from '../images/blackHeartIcon.svg';
@@ -65,7 +66,7 @@ function DetailsFood() {
   }, [renderDetailsFood]);
 
   const opts = {
-    height: '300',
+    height: '250',
     width: '300',
     playerVars: {
       autoplay: 0,
@@ -75,7 +76,7 @@ function DetailsFood() {
   function drinksCarousel() {
     return (
       <div className="container-carousel">
-        <p>Recommendations to this recipe</p>
+        <p className="carousel-title">Recommendations</p>
         <div className="carousel-scroll">
           {recommendationFood.map((drink, index) => (
             <div
@@ -88,7 +89,10 @@ function DetailsFood() {
                 src={ drink.strDrinkThumb }
                 alt="Compartilhar Receita"
               />
-              <p className="card-name" data-testid={ `${index}-recomendation-title` }>
+              <p
+                className="card-name"
+                data-testid={ `${index}-recomendation-title` }
+              >
                 {drink.strDrink}
               </p>
             </div>
@@ -103,50 +107,91 @@ function DetailsFood() {
 
     if (inProgressKey) {
       return (
-        <button className="btn-details" type="button" data-testid="start-recipe-btn">
+        <button
+          className="btn-details"
+          type="button"
+          data-testid="start-recipe-btn"
+        >
           Continue Recipe
         </button>
       );
     }
     return (
-      <button className="btn-details" type="button" data-testid="start-recipe-btn">
+      <button
+        className="btn-details"
+        type="button"
+        data-testid="start-recipe-btn"
+      >
         Start Recipe
       </button>
     );
   }
 
   return (
-    <main>
-      <img
-        src={ renderDetailsFood.strMealThumb }
-        style={ { width: '10%' } }
-        alt="Foto da Receita"
-        data-testid="recipe-photo"
-      />
-      <h3 data-testid="recipe-title">{renderDetailsFood.strMeal}</h3>
-      <button type="button" data-testid="share-btn">
-        <img src={ shareIcon } alt="Compartilhar Receita" />
-      </button>
-      <button type="button" data-testid="favorite-btn">
-        <img src={ whiteHeartIcon } alt="Favoritar Receita" />
-      </button>
-      <p data-testid="recipe-category">{renderDetailsFood.strCategory}</p>
-      {measureFood.length > 0
-        && measureFood.map((measure, index) => (
-          <p key={ index } data-testid={ `${index}-ingredient-name-and-measure` }>
-            {ingredientsFood[index]}
-            {' '}
-            {measure}
-          </p>
-        ))}
-      <p />
-      <p data-testid="instructions">{renderDetailsFood.strInstructions}</p>
-      <div data-testid="video">
-        <YouTube videoId={ videoId } opts={ opts } />
-      </div>
-      {drinksCarousel()}
-      {buttonRecipe()}
-    </main>
+    <>
+      <header>
+        <Header />
+      </header>
+      <main className="container-details-food">
+        <img
+          src={ renderDetailsFood.strMealThumb }
+          className="details-image-recipe"
+          alt="Foto da Receita"
+          data-testid="recipe-photo"
+        />
+        <div className="container-recipe-introduction">
+          <button className="btn-share" type="button" data-testid="share-btn">
+            <img
+              className="share-icon"
+              src={ shareIcon }
+              alt="Compartilhar Receita"
+            />
+          </button>
+          <div className="recipe-name-category">
+            <h3 className="recipe-name" data-testid="recipe-title">
+              {renderDetailsFood.strMeal}
+            </h3>
+            <div className="recipe-line" />
+            <p className="recipe-category" data-testid="recipe-category">
+              {renderDetailsFood.strCategory}
+            </p>
+          </div>
+          <button
+            className="btn-favorite"
+            type="button"
+            data-testid="favorite-btn"
+          >
+            <img
+              className="favorite-icon"
+              src={ whiteHeartIcon }
+              alt="Favoritar Receita"
+            />
+          </button>
+        </div>
+        <div className="container-ingredient">
+          {measureFood.length > 0
+            && measureFood.map((measure, index) => (
+              <p
+                className="ingredient-measure"
+                key={ index }
+                data-testid={ `${index}-ingredient-name-and-measure` }
+              >
+                {ingredientsFood[index]}
+                {' '}
+                <span>{measure}</span>
+              </p>
+            ))}
+        </div>
+        <div className="container-instructions">
+          <p data-testid="instructions">{renderDetailsFood.strInstructions}</p>
+        </div>
+        <div className="video-youtube" data-testid="video">
+          <YouTube videoId={ videoId } opts={ opts } />
+        </div>
+        {drinksCarousel()}
+        {buttonRecipe()}
+      </main>
+    </>
   );
 }
 
