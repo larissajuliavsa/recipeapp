ba77e40e0ca4e8c247155b11098f27ca558974c5
Larissa Araujo <larissa.jvsa@gmail.com>
2022-03-16 18:30:35 -0300

    Corrigindo css lint e finalizando header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,77 +29,98 @@ function Header(props) {
   }
 
   const searchBar = () => (
-    <>
-      <input
-        data-testid="search-input"
-        value={ inputText }
-        onChange={ ({ target: { value } }) => setInputText(value) }
-      />
-      <input
-        type="radio"
-        name="searchRadio"
-        data-testid="ingredient-search-radio"
-        label="Ingrediente"
-        onChange={ (e) => filterRadio(e) }
-      />
-      <input
-        type="radio"
-        name="searchRadio"
-        data-testid="name-search-radio"
-        label="Nome"
-        onChange={ (e) => filterRadio(e) }
-      />
-      <input
-        type="radio"
-        name="searchRadio"
-        data-testid="first-letter-search-radio"
-        label="Primeira Letra"
-        onChange={ (e) => filterRadio(e) }
-      />
-      <button
-        type="button"
-        data-testid="exec-search-btn"
-        onClick={ () => handleSubmit() }
-      >
-        Filtrar
-      </button>
-    </>
+    <div className="container-filters-bar">
+      <div className="container-bar">
+        <input
+          placeholder="Search Recipe"
+          className="bar"
+          data-testid="search-input"
+          value={ inputText }
+          onChange={ ({ target: { value } }) => setInputText(value) }
+        />
+      </div>
+      <section className="container-filters">
+        <div className="filter-ingredient">
+          <input
+            className="ingredient-radio"
+            type="radio"
+            name="searchRadio"
+            data-testid="ingredient-search-radio"
+            label="ingrediente"
+            onChange={ (e) => filterRadio(e) }
+          />
+          <p className="ingredient-title">Ingredient</p>
+        </div>
+        <div className="filter-name">
+          <input
+            className="name-radio"
+            type="radio"
+            name="searchRadio"
+            data-testid="name-search-radio"
+            label="nome"
+            onChange={ (e) => filterRadio(e) }
+          />
+          <p className="name-title">Name</p>
+        </div>
+        <div className="filter-first-letter">
+          <input
+            className="first-letter-radio"
+            type="radio"
+            name="searchRadio"
+            data-testid="first-letter-search-radio"
+            label="primeiraLetra"
+            onChange={ (e) => filterRadio(e) }
+          />
+          <p className="first-letter-title">First Letter</p>
+        </div>
+        <button
+          className="filter-btn"
+          type="button"
+          data-testid="exec-search-btn"
+          onClick={ () => handleSubmit() }
+        >
+          Search
+        </button>
+      </section>
+    </div>
   );
 
   return (
-    <header className="container-header">
-      <button
-        className="header-btn-search"
-        type="button"
-        onClick={ () => setShowBar(!showBar) }
-      >
-        <img
-          className="btn-search-icon"
-          data-testid="search-top-btn"
-          src={ searchIcon }
-          alt="searchIcon"
-        />
-      </button>
-      <h1 data-testid="page-title">{title}</h1>
-      <div className="container-title">
-        <h1 className="header-title" data-testid="page-title">Nome App</h1>
-        <div className="title-line" />
-      </div>
+    <>
+      <header className="container-header">
+        <div className="header-search-title-profile">
+          <button
+            className="header-btn-search"
+            type="button"
+            onClick={ () => setShowBar(!showBar) }
+          >
+            <img
+              className="btn-search-icon"
+              data-testid="search-top-btn"
+              src={ searchIcon }
+              alt="searchIcon"
+            />
+          </button>
+          <div className="container-title">
+            <h1 className="header-title" data-testid="page-title">{title}</h1>
+            <div className="title-line" />
+          </div>
+          <button
+            className="header-btn-profile"
+            type="button"
+            onClick={ toProfile }
+          >
+            <img
+              className="btn-profile-icon"
+              data-testid="profile-top-btn"
+              src={ profileIcon }
+              alt="profileIcon"
+            />
+          </button>
+        </div>
+      </header>
       { showBar && searchBar() }
-      <button
-        className="header-btn-profile"
-        type="button"
-        onClick={ toProfile }
-      >
-        <img
-          className="btn-profile-icon"
-          data-testid="profile-top-btn"
-          src={ profileIcon }
-          alt="profileIcon"
-        />
-      </button>
-      { showBar && searchBar() }
-    </header>
+    </>
   );
 }
 
